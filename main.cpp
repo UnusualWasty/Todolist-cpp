2d83e2ec0e505d90df5d2c3200ee3ee0b0127f65
UnusualWasty <97692301+UnusualWasty@users.noreply.github.com>
2026-05-19 09:27:28 +0200

    update 19.05 9:27
    Naprawiono funkcje dodawanieZadan oraz wyswietlanieZadan

--- main.cpp
+++ main.cpp
@@ -8,70 +8,70 @@ using namespace std;
 
 class operacjeNaPlikach {
 public:
-        void odczyt(int& zadania, vector<string>& lista) {
-            string plik;
-            cout << "Pamiętaj proszę, iż w celu poprawnego odczytu pliku, musi być on odpowiednio sformatowany. Przykład:\nPrzykładowe zadanie 1\nPrzykładowe zadanie 2\nPrzykładowe zadanie 3" << endl;
-            cout << "Podaj nazwę pliku: ";
-            getline(cin, plik);
-            string linia;
-            ifstream otwieranie;
-            otwieranie.open(plik);
-            if (!otwieranie.is_open()) {
-                cout << "==============BŁĄD==============\nNie można otworzyć pliku '" << plik << "'." << endl;
-                return;
-            }
-            while (getline(otwieranie, linia)) {
-                lista.push_back(linia);
-                zadania++;
-            }
-            cout << "Zaimportowano " << zadania << " zadania z pliku " << plik << "." << endl;
+    void odczyt(int& zadania, vector<string>& lista) {
+        string plik;
+        cout << "Pamiętaj proszę, iż w celu poprawnego odczytu pliku, musi być on odpowiednio sformatowany. Przykład:\nPrzykładowe zadanie 1\nPrzykładowe zadanie 2\nPrzykładowe zadanie 3" << endl;
+        cout << "Podaj nazwę pliku: ";
+        getline(cin, plik);
+        string linia;
+        ifstream otwieranie;
+        otwieranie.open(plik);
+        if (!otwieranie.is_open()) {
+            cout << "==============BŁĄD==============\nNie można otworzyć pliku '" << plik << "'." << endl;
+            return;
         }
-        void zapis (int zadania, const vector<string> lista) {
-            string decyzja;
-            cout << "\nCzy chcesz aby twoje zadania zostały zapisane w osobnym pliku .csv? (T/N)" << endl;
-            getline(cin, decyzja);
-            if (decyzja == "T" || decyzja == "t") {
-                string decyzjaNazwa;
-                cout << "Czy chcesz nazwać swój plik?(T/N)" << endl;
-                getline(cin, decyzjaNazwa);
-                if (decyzjaNazwa == "N" || decyzjaNazwa == "n") {
-                    time_t timestamp;
-                    time(&timestamp);
-                    string plikData = ctime(&timestamp);
+        while (getline(otwieranie, linia)) {
+            lista.push_back(linia);
+            zadania++;
+        }
+        cout << "Zaimportowano " << zadania << " zadania z pliku " << plik << "." << endl;
+    }
+    void zapis (int zadania, const vector<string> lista) {
+        string decyzja;
+        cout << "\nCzy chcesz aby twoje zadania zostały zapisane w osobnym pliku .csv? (T/N)" << endl;
+        getline(cin, decyzja);
+        if (decyzja == "T" || decyzja == "t") {
+            string decyzjaNazwa;
+            cout << "Czy chcesz nazwać swój plik?(T/N)" << endl;
+            getline(cin, decyzjaNazwa);
+            if (decyzjaNazwa == "N" || decyzjaNazwa == "n") {
+                time_t timestamp;
+                time(&timestamp);
+                string plikData = ctime(&timestamp);
 
-                    for (char& c : plikData) {
-                        if (c == ' ' || c == ':' || c == '\n') c = ' ';
-                    }
-                    string filename = "Todo list "+plikData+".csv";
+                for (char& c : plikData) {
+                    if (c == ' ' || c == ':' || c == '\n') c = ' ';
+                }
+                string filename = "Todo list "+plikData+".csv";
+                ofstream outfile(filename);
+
+                for (int i = 0; i < zadania; i++) {
+                    outfile << lista[i] << "\n";
+                }
+                cout << "Plik został zapisany pod nazwą '" << filename << "'."<< endl;
+
+            }else if (decyzjaNazwa == "T" || decyzjaNazwa == "t") {
+                string NazwaPliku;
+                cout << "UWAGA! PAMIĘTAJ O ODPOWIEDNICH ZNAKACH WSPIERANYCH PRZEZ WINDOWS!\nPodaj nazwę pliku: " << endl;
+                getline(cin, NazwaPliku);
+
+                string nielegalne = "<>:\"/\\|?*";
+                if (NazwaPliku.find_first_of(nielegalne) != string::npos) {
+                    cout << "==============BŁĄD==============\nPodana nazwa zawiera znaki niedozwolone w systemie Windows. Spróbuj ponownie."<< endl;
+                    return;
+                }else {
+                    string filename = NazwaPliku + ".csv";
                     ofstream outfile(filename);
-
                     for (int i = 0; i < zadania; i++) {
                         outfile << lista[i] << "\n";
                     }
-                    cout << "Plik został zapisany pod nazwą '" << filename << "'."<< endl;
-
-                }else if (decyzjaNazwa == "T" || decyzjaNazwa == "t") {
-                    string NazwaPliku;
-                    cout << "UWAGA! PAMIĘTAJ O ODPOWIEDNICH ZNAKACH WSPIERANYCH PRZEZ WINDOWS!\nPodaj nazwę pliku: " << endl;
-                    getline(cin, NazwaPliku);
-
-                    string nielegalne = "<>:\"/\\|?*";
-                    if (NazwaPliku.find_first_of(nielegalne) != string::npos) {
-                        cout << "==============BŁĄD==============\nPodana nazwa zawiera znaki niedozwolone w systemie Windows. Spróbuj ponownie."<< endl;
-                        return;
-                    }else {
-                        string filename = NazwaPliku + ".csv";
-                        ofstream outfile(filename);
-                        for (int i = 0; i < zadania; i++) {
-                            outfile << lista[i] << "\n";
-                        }
-                        cout << "Plik został zapisany pod nazwą " << filename << "."<< endl;
-                    }
+                    cout << "Plik został zapisany pod nazwą " << filename << "."<< endl;
                 }
-            }else {
-
             }
+        }else {
+
         }
+    }
 };
 
 class operacjeNaLiscie {
@@ -95,8 +95,60 @@ public:
             cout << "\n==============ZADANIA DO WYKONANIA==============\n" ;
             cout << "Twoja lista zadań jest obecnie pusta." << endl;
         }else{
+            cout << "\n==============ZADANIA DO WYKONANIA==============\n" ;
             for (int i = 0; i < zadania; i++) {
                 cout << i + 1 << ": " << lista[i] << endl;
+            }
+        }
+    }
+    void dodawanieZadan(int& zadania, vector<string>& lista) {
+        int liczbaDodan = 0;
+        cout << "Podaj ile zadań chcesz dodać: " << endl;
+        cin >> liczbaDodan;
+        cin.ignore();
+        if (liczbaDodan == 0) {
+            cout << "Podano 0 dodań do listy, program powróci do menu wyboru akcji." << endl;
+            return;
+        }else{
+            int decyzjaDodania;
+            cout << "Chcesz dodać element na:\n1.Konkretne miejsce listy,\n2.Koniec listy?" << endl;
+            cin >> decyzjaDodania;
+            cin.ignore();
+            if (decyzjaDodania == 1) {
+                for (int i = 1; i < liczbaDodan; i++) {
+                    string dodane;
+                    int miejsce;
+
+                    cout << "Podaj " << i << " zadanie: " << endl;
+                    getline(cin, dodane);
+
+                    cout << "Podaj miejsce w liście dla elementu '" << dodane << "' (1-" << zadania + 1 << "): " << endl;
+                    cin >> miejsce;
+                    cin.ignore();
+
+                    if (miejsce < 1 || miejsce > zadania + 1) {
+                        cout << "Nieprawidłowe miejsce, element zostanie dodany na koniec listy." << endl;
+                        lista.push_back(dodane);
+                    } else {
+                        lista.insert(lista.begin() + miejsce - 1, dodane);
+                    }
+                    zadania++;
+                }
+                cout << "Pomyślnie dodano elementy do listy.";
+            }else if (decyzjaDodania == 2) {
+                for (int i = 0; i < liczbaDodan; i++) {
+                    string dodane;
+
+                    cout << "Podaj " << zadania + 1 << " zadanie: " << endl;
+                    getline(cin, dodane);
+
+                    lista.push_back(dodane);
+                    zadania++;
+                }
+                cout << "Pomyślnie dodano elementy do listy.\n";
+            }else {
+                cout << "Podano nieprawidłową wartość, spróbuj ponownie";
+                return;
             }
         }
     }
@@ -139,7 +191,17 @@ int main() {
             }
             case 'E':
             case 'e': {
-                //to be done
+                int akcja2 = 0;
+                cout << "Co chcesz zrobić?\n1. Dodać element do listy\n2. Usunąć element z listy" << endl;
+                cin >> akcja2;
+                cin.ignore();
+                if (akcja2 == 1) {
+                    listy.dodawanieZadan(globalZadania, globalLista);
+                }else if (akcja2 == 2) {
+
+                }else {
+                    cout << "Podano niewłaściwą wartość, program wróci do menu wyboru akcji." << endl;
+                }
                 break;
             }
             case 'Z':
